1fa18bc38d5cd940cfaeafa95c9d875c0f655c7c
Benziza <benizizamohamed@gmail.com>
2022-09-06 17:27:36 +0100

    CrudRepository mainly provides CRUD (Create, Read, Update, Delete) operations.

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,77 +1,70 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "employee")
 public class Employee {
 
-    @Entity
-    @Table(name = "employee")
-    public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    private String country;
+    private String role;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private long id;
-        private String name;
-        private String email;
-        private String country;
-        private String role;
+    public Employee() {
+        super();
+    }
 
-        public Employee() {
-            super();
-        }
+    public Employee(String name, String email,
+                    String country, String role) {
 
-        public Employee(String name, String email,
-                        String country, String role) {
+        this.name = name;
+        this.email = email;
+        this.country = country;
 
-            this.name = name;
-            this.email = email;
-            this.country = country;
+        this.role = role;
+    }
 
-            this.role = role;
-        }
+    public String getRole() {
+        return role;
+    }
 
-        public String getRole() {
-            return role;
-        }
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-        public void setRole(String role) {
-            this.role = role;
-        }
+    public long getId() {
+        return id;
+    }
 
-        public long getId() {
-            return id;
-        }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-        public void setId(long id) {
-            this.id = id;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public String getCountry() {
+        return country;
+    }
 
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
---
+++ src/main/java/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import model.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository
+        extends CrudRepository<Employee, Long> {
+
+}
