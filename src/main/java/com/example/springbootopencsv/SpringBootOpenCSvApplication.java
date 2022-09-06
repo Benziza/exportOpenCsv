75196de997604059be717a1fc14eec93b5809d58
Benziza <benizizamohamed@gmail.com>
2022-09-06 17:17:54 +0100

    init

---
+++ src/main/java/com/example/springbootopencsv/SpringBootOpenCSvApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootopencsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenCSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenCSvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootopencsv/SpringBootOpenCSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootopencsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenCSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
